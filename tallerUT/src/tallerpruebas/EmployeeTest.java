6fcecd6173902c64549e344d479ddeedf75be291
Cliente CONTROL-PC <CltControl@GGIECUT1WRK015.espol.edu.ec>
2020-01-20 10:58:02 -0500

    Create test class

---
+++ tallerUT/src/tallerpruebas/EmployeeTest.java
@@ -0,0 +1,19 @@
+package tallerpruebas;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmployeeTest {
+
+	@Test
+	void testCs() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCalculateYearBonus() {
+		fail("Not yet implemented");
+	}
+
+}
