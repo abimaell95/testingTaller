c46c8e2e194e62700ee5c597135a8675a3e60f82
Eduardo Gonzalez <eduardogonzabay@gmail.com>
2020-01-20 23:36:09 -0500

    Metodo testing del metodo CS implementado

--- tallerUT/src/tallerpruebas/EmployeeTest.java
+++ tallerUT/src/tallerpruebas/EmployeeTest.java
@@ -1,14 +1,79 @@
 package tallerpruebas;
 
+import tallerpruebas.Employee;
+import tallerpruebas.EmployeeType;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 class EmployeeTest {
 
+//Validando en USD cuando el empleado es un Worker con un margen de error de +-0.2
 	@Test
-	void testCs() {
-		fail("Not yet implemented");
+	void testCSWorkerUSB() {
+		Employee testEmpleado = new Employee(400, "USD", 0.5f, EmployeeType.Worker);
+
+		float resultadoReal = testEmpleado.cs();
+		float resultadoEsperado = 464.33f;
+
+		assertEquals(resultadoReal, resultadoEsperado, 0.2);
 	}
 
+//Validando en USD cuando el empleado es un Manager con un margen de error de +-0.2
+	@Test
+	void testCSManagerUSB() {
+		Employee testEmpleado = new Employee(400, "USD", 0.5f, EmployeeType.Manager);
+
+		float resultadoReal = testEmpleado.cs();
+		float resultadoEsperado = 464.508336f;
+
+		assertEquals(resultadoReal, resultadoEsperado, 0.2);
+	}
+
+
+//Validando en USD cuando el empleado es un Supervisor con un margen de error de +-0.2
+	@Test
+	void testCSSupervisorUSB() {
+		Employee testEmpleado = new Employee(400, "USD", 0.5f, EmployeeType.Supervisor);
+
+		float resultadoReal = testEmpleado.cs();
+		float resultadoEsperado = 464.68f;
+
+		assertEquals(resultadoReal, resultadoEsperado, 0.2);
+	}
+
+//Validando en otro tipo de moneda cuando el empleado es un Worker con un margen de error de +-0.2
+		@Test
+		void testCSWorker() {
+			Employee testEmpleado = new Employee(400, "EUR", 0.5f, EmployeeType.Worker);
+
+			float resultadoReal = testEmpleado.cs();
+			float resultadoEsperado = 444.33f;
+
+			assertEquals(resultadoReal, resultadoEsperado, 0.2);
+		}
+
+//Validando en otro tipo de moneda cuando el empleado es un Manager con un margen de error de +-0.2
+		@Test
+		void testCSManager() {
+			Employee testEmpleado = new Employee(400, "EUR", 0.5f, EmployeeType.Manager);
+
+			float resultadoReal = testEmpleado.cs();
+			float resultadoEsperado = 444.508336f;
+
+			assertEquals(resultadoReal, resultadoEsperado, 0.2);
+		}
+
+
+//Validando en otro tipo de moneda cuando el empleado es un Supervisor con un margen de error de +-0.2
+		@Test
+		void testCSSupervisor() {
+			Employee testEmpleado = new Employee(400, "EUR", 0.5f, EmployeeType.Supervisor);
+
+			float resultadoReal = testEmpleado.cs();
+			float resultadoEsperado = 444.68f;
+
+			assertEquals(resultadoReal, resultadoEsperado, 0.2);
+		}
 }
