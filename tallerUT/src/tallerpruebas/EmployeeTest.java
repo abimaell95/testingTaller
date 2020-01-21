771a021cc0017bd8a1aafa2b772f40bcf7f72606
abimaell95 <gcemas20@gmail.com>
2020-01-21 00:22:58 -0500

    add new test

--- tallerUT/src/tallerpruebas/EmployeeTest.java
+++ tallerUT/src/tallerpruebas/EmployeeTest.java
@@ -96,4 +96,85 @@ class EmployeeTest {
 		float resultadoEsperado = 0.0f;
 		assertEquals(resultadoReal, resultadoEsperado, 0.2);
 	}
+
+
+//Validando testBonusWorker-USD
+		@Test
+		void testCalculateYearBonusWorker() {
+			Employee empleado = new Employee(350.0f,"USD",0.1f,EmployeeType.Worker);
+			
+			float resultadoReal = empleado.CalculateYearBonus();
+			float resultadoEsperado = (float) 386.0;
+			
+			assertEquals(resultadoReal, resultadoEsperado,0.1);
+		}
+		
+//Validando testBonusWorker-OtraMoneda
+		@Test
+		void testCalculateYearBonusWorkerOtherCurrency() {
+			Employee empleado = new Employee(150.0f,"EUR",0.1f,EmployeeType.Worker);
+			
+			float resultadoReal = empleado.CalculateYearBonus();
+			float resultadoEsperado = (float) 386.0;
+			
+			assertEquals(resultadoReal, resultadoEsperado,0.1);
+		}
+		
+//Validando testBonusSupervisor-USD
+		@Test
+		void testCalculateYearBonusSupervisor() {
+			Employee empleado = new Employee(150.0f,"USD",0.1f,EmployeeType.Supervisor);
+			
+			float resultadoReal = empleado.CalculateYearBonus();
+			float resultadoEsperado = (float) 343.0;
+			
+			assertEquals(resultadoReal, resultadoEsperado,0.1);
+		}
+		
+//Validando testBonusSupervisor-OtraMoneda
+		@Test
+		void testCalculateYearBonusSupervisorOtherCurrency() {
+			Employee empleado = new Employee(100.0f,"EUR",0.1f,EmployeeType.Supervisor);
+			
+			float resultadoReal = empleado.CalculateYearBonus();
+			float resultadoEsperado = (float) 288.0;
+			
+			assertEquals(resultadoReal, resultadoEsperado,0.1);
+		}
+
+//Validando testBonusManager-USD
+		@Test
+		void testCalculateYearBonusManager() {
+			Employee empleado = new Employee(300.0f,"USD",0.1f,EmployeeType.Manager);
+			
+			float resultadoReal = empleado.CalculateYearBonus();
+			float resultadoEsperado = (float) 686.0;
+			
+			assertEquals(resultadoReal, resultadoEsperado,0.1);
+		}
+
+//Validando testBonusManger-OtraMoneda
+		@Test
+		void testCalculateYearBonusManagerOtherCurrency() {
+			Employee empleado = new Employee(150.0f,"EUR",0.1f,EmployeeType.Manager);
+			
+			float resultadoReal = empleado.CalculateYearBonus();
+			float resultadoEsperado = (float) 528.5;
+			
+			assertEquals(resultadoReal, resultadoEsperado,0.1);
+		}
+		
+//Validando tipo de empleado fuera de rango
+		@Test
+		void testCalculateYearBonusNoType() {
+			Employee empleado = new Employee(150.0f,"EUR",0.1f,EmployeeType.Other);
+			
+			float resultadoReal = empleado.CalculateYearBonus();
+			float resultadoEsperado = (float) 0.0;
+			
+			assertEquals(resultadoReal, resultadoEsperado,0.1);
+		}
+				
+				
+
 }
